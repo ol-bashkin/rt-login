7e7832a6ce8522c09b413ff4910062ba735fa829
Олег Башкин <ol.bashkin@gmail.com>
2019-12-01 21:16:27 +0300

    вынес настройки webpack в отдельную папку

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,13 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
+
+module.exports = merge(common, {
+    mode: "development",
+    devServer: {
+        contentBase: path.join(__dirname, "../public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+        hotOnly: true,
+    },
+});
